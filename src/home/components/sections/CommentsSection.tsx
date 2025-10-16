150abd6be5f3bcc1d0817c0fb89fd3f75dbf3e1f
Karol Nowakowski <przeslijmi@gmail.com>
2025-10-16 13:17:02 +0200

    real comments

--- src/home/components/sections/CommentsSection.tsx
+++ src/home/components/sections/CommentsSection.tsx
@@ -1,6 +1,6 @@
 import HomeSection from '@home/components/HomeSection';
 import { comments } from '@home/constants/comments';
-import { Star } from 'lucide-react';
+import { Maximize2, Star } from 'lucide-react';
 
 interface Props {}
 
@@ -12,17 +12,27 @@ export default function CommentsSection({}: Props) {
       title="What People Say"
       titleClass="text-gray-900 dark:text-gray-100"
       children={
-        <div className="grid md:grid-cols-3 gap-8">
-          {comments.map((c, i) => (
-            <div key={i} className="p-8 rounded-2xl shadow-xl bg-white dark:bg-gray-800">
-              <div className="flex items-center gap-1 mb-4">
-                {[...Array(c.rate)].map((_, ri) => (
-                  <Star key={ri} className="w-5 h-5 fill-yellow-400 text-yellow-500 dark:text-yellow-400" />
-                ))}
+        <div className="grid md:grid-cols-2 gap-8">
+          {comments.map((c, ci) => (
+            <div key={ci} className="group p-8 rounded-2xl shadow-xl bg-white dark:bg-gray-800">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-1 mb-4">
+                  {[...Array(c.rate)].map((_, ri) => (
+                    <Star key={ri} className="w-5 h-5 fill-yellow-400 text-yellow-500 dark:text-yellow-400" />
+                  ))}
+                </div>
+                <div className="border-black/10 border p-1 rounded-full bg-black/5  w-6 h-6 flex items-center justify-center">
+                  <Maximize2 size={15} className="text-black/60 dark:text-white/80" />
+                </div>
               </div>
-              <p className="mb-4 italic text-gray-700 dark:text-gray-300">
-                "{c.content}"
-              </p>
+              {c.contents.map((p, pi) => (
+                <p key={pi} className={`
+                  mb-4 italic text-gray-700 dark:text-gray-300
+                  ${pi === 0 ? 'block' : 'hidden group-hover:block'}
+                `}>
+                  "{p}"
+                </p>
+              ))}
               <div className="font-semibold text-gray-900 dark:text-gray-200">{c.name}</div>
               <div className="text-sm text-gray-600 dark:text-gray-400">{c.company}</div>
             </div>
--- src/home/constants/comments.ts
+++ src/home/constants/comments.ts
@@ -2,20 +2,20 @@
 export const comments = [
   {
     rate: 5,
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at odio at odio sollicitudin hendrerit id bibendum sem. Fusce quis.',
-    name: 'Sarah M.',
-    company: 'CTO, TechStart Inc.',
+    contents: [
+      'Karol is one of the most collaborative and effective full stack developer I\'ve had the pleasure of working with.',
+      'What truly sets him apart is how he blends deep technical expertise with outstanding communication skills. He\'s exceptionally good at quickly grasping complex business requirements—it\'s rare to find a developer who approaches the work with such a clear business mindset. Moreover, Karol is relentlessly hard-working, consistently driving projects forward and dedicating himself to achieving our shared goals. He\'s simply an incredibly valuable and easy colleague to collaborate with.',
+    ],
+    name: 'Emre A.',
+    company: 'Q&A Expert',
   },
   {
     rate: 5,
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at odio at odio sollicitudin hendrerit id bibendum sem. Fusce quis.',
-    name: 'Michael K.',
-    company: 'Product Manager, DataFlow',
-  },
-  {
-    rate: 5,
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at odio at odio sollicitudin hendrerit id bibendum sem. Fusce quis.',
-    name: 'Jennifer L.',
-    company: 'Founder, FitLife App',
+    contents: [
+      'Karol is a developer who thinks like a product owner — technically sharp, business-minded, and 100% dedicated to delivering value.',
+      'Karol is one of those rare developers who not only writes great code, but truly understands why it matters. His dedication, business sense, and focus on delivering customer value make him an invaluable team member - and one of the best developers I\'ve ever worked with',
+    ],
+    name: 'Peter J.',
+    company: 'Senior Product Manager',
   },
 ]
