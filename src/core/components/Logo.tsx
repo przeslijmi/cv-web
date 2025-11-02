94db025df34ff4c6cbf86ab4bbc349c72012d1c4
Karol Nowakowski <przeslijmi@gmail.com>
2025-11-02 20:18:34 +0100

    Make logo clickable to home

--- src/core/components/Logo.tsx
+++ src/core/components/Logo.tsx
@@ -1,4 +1,5 @@
 import LogoSvg from '@core/components/Logo/LogoSvg';
+import { Link } from 'react-router-dom';
 
 interface Props {
   big?: boolean;
@@ -9,18 +10,19 @@ export default function Logo({
 }: Props) {
 
   return (
-    <button
-      className={`
-        flex align-middle
-        font-bold text-transparent bg-clip-text bg-gradient-to-r transition-all
-        from-blue-600 to-cyan-600 hover:from-blue-700 hover:to-cyan-700
-        ${big ? 'text-3xl' : 'text-xl'}
-      `}
-    >
-      <div className="hidden md:block"><LogoSvg width={100} /></div>
-      <div className="md:hidden"><LogoSvg width={60} /></div>
-      <div className="pl-3 self-center">Karol Nowakowski</div>
-
-    </button>
+    <Link to="/">
+      <button
+        className={`
+          flex align-middle
+          font-bold text-transparent bg-clip-text bg-gradient-to-r transition-all
+          from-blue-600 to-cyan-600 hover:from-blue-700 hover:to-cyan-700
+          ${big ? 'text-3xl' : 'text-xl'}
+        `}
+      >
+        <div className="hidden md:block"><LogoSvg width={100} /></div>
+        <div className="md:hidden"><LogoSvg width={60} /></div>
+        <div className="pl-3 self-center">Karol Nowakowski</div>
+      </button>
+    </Link>
   );
 }
